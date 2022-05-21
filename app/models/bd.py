afc9961fb2a491a3915ca7b7b1a2a83b889b8c23
th39uan60 <oriongy_2002@hotmail.com>
2022-05-20 23:13:15 -0500

    Yaml ingress file & postgreSQL connection

--- app/models/bd.py
+++ app/models/bd.py
@@ -7,13 +7,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from ..security import decodificar_base64
 
+SRV_BD = decodificar_base64(getenv("SERVIDORBD"))
 USR_BD = decodificar_base64(getenv("USUARIOBD"))
 PSW_BD = decodificar_base64(getenv("PASSWORDBD"))
-SERVIDOR_BD = "localhost:5432"
-NOMBRE_BD = "prototype"
+NOMBRE_BD = "pythonarchetype"
 # aquí definimos la conexión al MDBS (ya sea postgres o sqlite)
-# DATABASE_URL = f"postgresql://{USR_BD}:{PSW_BD}@{SERVIDOR_BD}/{NOMBRE_BD}"
-DATABASE_URL = f"sqlite:///./possqlite.db"
+DATABASE_URL = f"postgresql://{USR_BD}:{PSW_BD}@{SRV_BD}/{NOMBRE_BD}"
+# DATABASE_URL = f"sqlite:///./possqlite.db"
 
 motor_bd = create_engine(DATABASE_URL)
 ModeloBase = declarative_base()
--- readme.txt
+++ readme.txt
@@ -1,6 +1,7 @@
 1. Los archivos *.yaml tienen que compartirse aparte de la imagen docker del proyecto python.
-2. Estos tendrán que agregarse como  recursos en kubernetes (en orden) usando:
+2. Estos tendrán que agregarse como recursos en kubernetes (en orden) usando:
 	kubectl apply -f namespace.yaml
 	kubectl apply -f secret.yaml -n python-arq
-	kubectl apply -f deployment.yaml
-	kubectl apply -f service.yaml
+	kubectl apply -f deployment.yaml -n python-arq
+	kubectl apply -f service.yaml -n python-arq
+	kubectl apply -f ingress.yaml -n python-arq
